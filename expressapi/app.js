94206f980abba24a6ba3254ef5eb64ea32c993df
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-10-16 13:43:03 -0400

    removed gitignore not needed. downloaded es6 template engine.

--- expressapi/app.js
+++ expressapi/app.js
@@ -1,4 +1,5 @@
 const express = require("express"),
+    es6Renderer = require("express-es6-template-engine"),
     compression = require("compression"),
     helmet = require("helmet"),
     app = express();
@@ -11,16 +12,21 @@ app.listen(3333, function() {
 app.use(compression());
 app.use(helmet());
 
+// ES6 Template Engine Express-ES6-Template Engine
+app.engine("html", es6Renderer);
+app.set("views", "./views");
+app.set("view engine", "html");
+
 // Creating routes
 // Every controller will talk to a model that populates a view
 const rootController = require("./routes/index.js"),
-allController = require("./routes/all.js"),
-wpController = require("./routes/wp.js"),
-personController = require("./routes/person.js")
+    allController = require("./routes/all.js"),
+    wpController = require("./routes/wp.js"),
+    personController = require("./routes/person.js");
 
 // Routes
 // Express USE the router that we've exported
 app.use("/", rootController);
-app.use("/all", allController); 
+app.use("/all", allController);
 app.use("/wp", wpController);
 app.use("/person", personController);
